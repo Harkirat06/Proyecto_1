0dce8b2a8da1bb482b8bb949cc039b60eede297d
Harkirat Singh <khalsaharkirat97@gmail.com>
2022-02-18 18:05:32 +0100

    React + Multer Completado

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
-import './App.css';
 import axios from 'axios';
 import { useState } from 'react';
 
 
 function App() {
   const [mensaje,setMensaje] = useState("")
+  const [doc,setDoc] = useState(null)
+  const [name,setName] = useState("")
+  
+  const onClick = (e) => {
+    const arr = Array.from(doc)
+    console.log(arr)
+    const data = new FormData()
+    arr.map((file)=>{
+    data.append("docs",file)
+    })
+    axios.post("/uploadFile", data).then((res) => console.log(res))
+  }
+  
   axios.get("/api").then(response => {
     const {data} = response
     setMensaje(data.message) 
   })
+
   return (
     <div className="Upload">
       <h1>{mensaje}</h1>
-      <div class="mb-3">
-        <label for="formFileMultiple" class="form-label">Multiple files input example</label>
-        <input class="form-control" type="file" id="formFileMultiple" multiple />
+      <form>
+      <div className="mb-3">
+        <input className="form-control" type="file" id="formFileMultiple"  name='docs' onChange={(e) => { 
+          setDoc(e.target.files)
+          setName(e.target.value)}} multiple />
+        <button type="button" className="btn btn-primary btn-lg btn-block" onClick={onClick}>Submit</button> 
       </div>
+      </form>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const dotenv = require('dotenv').config()
 const express = require('express')
 const multer  = require('multer')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 const server = express()
 
 server.use(express.json())
+
 const storage = multer.diskStorage({
-    destination: "./uploads/",
+    destination: "uploads/",
     filename: function (req, file, cb) {
       cb(
         null,
-        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+        file.originalname
       );
     },
   });
 const upload = multer({
     storage: storage,
     limits: { fileSize: 10000000000 },
-  }).array("docs", 10); 
+  }); 
 
-server.post("/uploads",(req,res)=>{
-    
+server.post("/uploadFile",upload.array('docs',10), async (req,res,next) => {
+  const {files} = req
+  console.log(files)  
+  if(!files){
+      res.sendStatus(404)
+    }else{
+      res.sendStatus(200)
+    }
 })
 
 server.get("/api",(req,res)=>{
